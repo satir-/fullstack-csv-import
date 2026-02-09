13f32555a9f5798f4be8a96b2558302cb6cc13ed
Oleksii Tsykunov <oleksii.tsykunov@gmail.com>
2026-02-09 19:53:14 +0100

    Integrate TypeORM with PostgreSQL and add ImportsModule to app configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ImportsModule } from './imports/imports.module';
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: process.env.DB_HOST || 'postgres', // Name of the docker container
+        port: 5432,
+        username: 'dev',
+        password: 'dev',
+        database: 'csv_import',
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        autoLoadEntities: true,
+        synchronize: true
+      }),
+      ImportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
